a160ea665f94361030894e7b5879eae04e1c81e6
appa-dot <=>
2024-06-05 21:12:53 +0200

    Revert "moved features to info.json"
    This reverts commit 5c8585bf63fd309b11740ad0c5f00b293d994bdb.

--- keymaps/vial/config.h
+++ keymaps/vial/config.h
@@ -4,3 +4,11 @@
 
 #define VIAL_UNLOCK_COMBO_ROWS { 0, 0 }
 #define VIAL_UNLOCK_COMBO_COLS { 0, 14 }
+
+#define ENCODERS_PAD_B { D1 }
+#define ENCODERS_PAD_A { D0 }
+#define ENCODER_RESOLUTION 2
+
+#define DEBOUNCE 10
+
+#define DYNAMIC_KEYMAP_LAYER_COUNT 2
